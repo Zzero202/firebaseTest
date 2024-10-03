4aaedd3e7625b8661cf1090ab2332b5d24207ef6
Zzero202 <ahmedfaisaly2000@gmail.com>
2024-10-03 10:50:05 +0300

    files under src

--- App.js
+++ App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import {NativeModules} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import NotificationScreen from './src/screens/NotificationScreen';
+const Stack = createStackNavigator();
+NativeModules.ImmersiveMode.enterImmersiveMode();
 
 function App() {
-  NativeModules.ImmersiveMode.enterImmersiveMode();
 
   return (
-    <View>
-      <Text>Hello</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName={'NotificationScreen'}
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen
+          name="LoginScreen"
+          component={NotificationScreen}
+          screenOptions={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ src/screens/NotificationScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+function NotificationScreen() {
+  return <Text>Hello</Text>;
+}
+
+export default NotificationScreen;

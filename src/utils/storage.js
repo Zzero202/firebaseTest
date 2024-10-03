9201323f8be4f50eafe624f03449059a7a893681
Zzero202 <ahmedfaisaly2000@gmail.com>
2024-10-03 10:36:38 +0300

    mmkv configuration file

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -1,3 +1,24 @@
 import {MMKV} from 'react-native-mmkv';
 
 export const storage = new MMKV();
+
+export function saveDataToStorage(key, value) {
+  try {
+    storage.set(key, value);
+  } catch (e) {
+    return e;
+  }
+  return true;
+}
+export function getNumberFromStorage(key) {
+  const value = storage.getNumber(key);
+  return value;
+}
+export function getStringFromStorage(key) {
+  const value = storage.getString(key);
+  return value;
+}
+export function getBooleanFromStorage(key) {
+  const value = storage.getBoolean(key);
+  return value;
+}
